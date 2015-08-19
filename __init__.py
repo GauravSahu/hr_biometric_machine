1a28eb13ac97d75daa6fb2f9de8b02e6a98aae49
administrator <administrator@ho41.jayanita.jayanita.net>
2015-08-19 17:03:27 +0530

    initial commit

---
+++ __init__.py
@@ -0,0 +1,1 @@
+import biometric_machine
---
+++ __openerp__.py
@@ -0,0 +1,13 @@
+{
+    "name" : "Biometric Device Integration",
+    "version" : "1.0",
+    "author" : "Gaurav Sahu",
+    "category" : "Generic Modules/Others",
+    "website" : "gauravsahu.odoo.com",
+    "description": "A Module for Biometric Device Integration ",
+    "depends" : ["base","hr"],
+    "init_xml" : [],
+    "data" : ["biometric_machine_view.xml"],
+    "active": True,
+    "installable": True
+}
