b8ae73730537ddda4c9d2b842282eccb9440e5d3
Randall Castro <randall_castro@me.com>
2018-02-16 11:49:05 -0600

    [FIX] hr_biometric_machine: plylint checks

--- hr_biometric_machine/models/biometric_machine.py
+++ hr_biometric_machine/models/biometric_machine.py
@@ -1,11 +1,9 @@
 # -*- coding: utf-8 -*-
 
 from openerp.osv import fields, osv
-from datetime import datetime , timedelta
-from zklib import zklib
 from openerp.tools.translate import _
-import time
-from zklib import zkconst
+from datetime import datetime, timedelta
+from zklib import zklib
 
 
 class hr_employee(osv.Model):
@@ -21,17 +19,17 @@ class hr_employee(osv.Model):
 class biometric_machine(osv.Model):
     _name= 'biometric.machine'
     _columns = {
-        'name' : fields.char("Machine IP"),
-        'ref_name' : fields.char("Location"),
+        'name': fields.char("Machine IP"),
+        'ref_name': fields.char("Location"),
         'port': fields.integer("Port Number"),
-        'address_id' : fields.many2one("res.partner",'Working Address'),
-        'company_id': fields.many2one("res.company","Company Name"),
-        'atten_ids' : fields.one2many('biometric.data','mechine_id','Attendance')
+        'address_id': fields.many2one("res.partner", 'Working Address'),
+        'company_id': fields.many2one("res.company", "Company Name"),
+        'atten_ids': fields.one2many('biometric.data', 'mechine_id', 'Attendance')
     }
 
     def download_attendance(self, cr, uid, ids, context=None):
-        machine_ip = self.browse(cr,uid,ids).name
-        port = self.browse(cr,uid,ids).port
+        machine_ip = self.browse(cr, uid, ids).name
+        port = self.browse(cr, uid, ids).port
         zk = zklib.ZKLib(machine_ip, int(port))
         res = zk.connect()
         if res == True:
@@ -43,34 +41,34 @@ class biometric_machine(osv.Model):
             biometric_data = self.pool.get("biometric.data")
             if (attendance):
                 for lattendance in attendance:
-                    time_att = str(lattendance[2].date()) + ' ' +str(lattendance[2].time())
+                    time_att = str(lattendance[2].date()) + ' ' + str(lattendance[2].time())
                     atten_time1 = datetime.strptime(str(time_att), '%Y-%m-%d %H:%M:%S')
                     atten_time = atten_time1 - timedelta(hours=5,minutes=30)
-                    atten_time = datetime.strftime(atten_time,'%Y-%m-%d %H:%M:%S')
-                    atten_time1 = datetime.strftime(atten_time1,'%Y-%m-%d %H:%M:%S')
-                    in_time = datetime.strptime(atten_time1,'%Y-%m-%d %H:%M:%S').time()
+                    atten_time = datetime.strftime(atten_time, '%Y-%m-%d %H:%M:%S')
+                    atten_time1 = datetime.strftime(atten_time1, '%Y-%m-%d %H:%M:%S')
+                    in_time = datetime.strptime(atten_time1, '%Y-%m-%d %H:%M:%S').time()
 
                     time_new = str(in_time)
-                    time_new = time_new.replace(":",".",1)
+                    time_new = time_new.replace(":", ".", 1)
                     time_new = time_new[0:5]
-                    print time_att,lattendance[0]
+                    print time_att, lattendance[0]
                     try:
-                        del_atten_ids = biometric_data.search(cr,uid,[('emp_code','=',str(lattendance[0])),('name','=',atten_time)])
+                        del_atten_ids = biometric_data.search(cr, uid, [('emp_code', '=', str(lattendance[0])), ('name', '=', atten_time)])
                         if del_atten_ids:
-                            # hr_attendance.unlink(cr,uid,del_atten_ids)
+                            # hr_attendance.unlink(cr, uid, del_atten_ids)
                             continue
                         else:
                             print "Date %s, Name %s: %s" % ( lattendance[2].date(), lattendance[2].time(), lattendance[0] )
-                            a = biometric_data.create(cr,uid,{'name':atten_time,'emp_code':lattendance[0],'mechine_id':ids[0]})
+                            a = biometric_data.create(cr, uid, {'name':atten_time, 'emp_code': lattendance[0], 'mechine_id': ids[0]})
                             print a
-                    except Exception,e:
+                    except Exception, e:
                         pass
                         print "exception..Attendance creation======", e.args
             zk.enableDevice()
             zk.disconnect()
             return True
         else:
-            raise osv.except_osv(_('Warning !'),_("Unable to connect, please check the parameters and network connections."))
+            raise osv.except_osv(_('Warning !'), _("Unable to connect, please check the parameters and network connections."))
 
     #Dowload attendence data regularly
     def schedule_download(self, cr, uid, context=None):
@@ -78,16 +76,16 @@ class biometric_machine(osv.Model):
             scheduler_line_obj = self.pool.get('biometric.machine')
             scheduler_line_ids = self.pool.get('biometric.machine').search(cr, uid, [])
             for scheduler_line_id in scheduler_line_ids:
-                scheduler_line =scheduler_line_obj.browse(cr, uid,scheduler_line_id,context=None)
+                scheduler_line =scheduler_line_obj.browse(cr, uid, scheduler_line_id, context=None)
                 try:
                     scheduler_line.download_attendance()
                 except:
-                    raise osv.except_osv(('Warning !'),("Machine with %s is not connected" %(scheduler_line.name)))
+                    raise osv.except_osv(('Warning !'), ("Machine with %s is not connected" %(scheduler_line.name)))
 
 
     def clear_attendance(self, cr, uid, ids, context=None):
-        machine_ip = self.browse(cr,uid,ids).name
-        port = self.browse(cr,uid,ids).port
+        machine_ip = self.browse(cr, uid, ids).name
+        port = self.browse(cr, uid, ids).port
         zk = zklib.ZKLib(machine_ip, int(port))
         res = zk.connect()
         if res == True:
@@ -98,13 +96,13 @@ class biometric_machine(osv.Model):
             zk.disconnect()
             return True
         else:
-            raise osv.except_osv(_('Warning !'),_("Unable to connect, please check the parameters and network connections."))
+            raise osv.except_osv(_('Warning !'), _("Unable to connect, please check the parameters and network connections."))
 
 
 class biometric_data(osv.osv):
     _name = "biometric.data"
     _columns = {
-        'name' : fields.datetime('Date'),
-        'emp_code' : fields.char('Employee Code'),
-        'mechine_id' : fields.many2one('biometric.machine','Mechine No')
+        'name': fields.datetime('Date'),
+        'emp_code': fields.char('Employee Code'),
+        'mechine_id': fields.many2one('biometric.machine', 'Mechine No')
     }
--- hr_biometric_machine/wizard/schedule_wizard.py
+++ hr_biometric_machine/wizard/schedule_wizard.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from openerp.osv import fields, osv
 
 
@@ -6,16 +8,16 @@ class configure_attendence(osv.osv_memory):
     _name = "configure.attendence"
     _columns = {
         'interval_number': fields.integer(string="Interval Number"),
-        'interval_type': fields.selection([('minutes','Minutes'),('hours','Hours'),('work_days','Work Days'),('days','Days'),('weeks','Weeks'),('months','Months')],string="Interval Unit"),
+        'interval_type': fields.selection([('minutes', 'Minutes'), ('hours', 'Hours'), ('work_days', 'Work Days'), ('days', 'Days'), ('weeks', 'Weeks'), ('months', 'Months')], string = "Interval Unit"),
     }
 
-    def _get_interval_number(self,cr, uid, context):
-        line_id = self.pool.get('ir.cron').search(cr,uid,[('name','ilike','Download Attendence')])
-        return self.pool.get('ir.cron').browse(cr,uid,line_id).interval_number
+    def _get_interval_number(self, cr, uid, context):
+        line_id = self.pool.get('ir.cron').search(cr, uid, [('name', 'ilike', 'Download Attendence')])
+        return self.pool.get('ir.cron').browse(cr, uid, line_id).interval_number
 
-    def _get_interval_type(self,cr, uid, context):
-        line_id = self.pool.get('ir.cron').search(cr,uid,[('name','ilike','Download Attendence')])
-        return self.pool.get('ir.cron').browse(cr,uid,line_id).interval_type
+    def _get_interval_type(self, cr, uid, context):
+        line_id = self.pool.get('ir.cron').search(cr, uid, [('name', 'ilike', 'Download Attendence')])
+        return self.pool.get('ir.cron').browse(cr, uid, line_id).interval_type
 
 
     _defaults = {
@@ -23,15 +25,14 @@ class configure_attendence(osv.osv_memory):
         'interval_type': _get_interval_type
     }
 
-    def update_interval(self,cr,uid,ids,context):
+    def update_interval(self, cr, uid, ids, context):
  
-        for line in self.browse(cr,uid,ids):
+        for line in self.browse(cr, uid, ids):
             interval_number = line.interval_number
             interval_type = line.interval_type
 
-        line_id = self.pool.get('ir.cron').search(cr,uid,[('name','ilike','Download Attendence')])
-        schedule_obj = self.pool.get('ir.cron').browse(cr,uid,line_id)
-        schedule_obj.write({'interval_number': interval_number,'interval_type':interval_type})
-
+        line_id = self.pool.get('ir.cron').search(cr, uid, [('name', 'ilike', 'Download Attendence')])
+        schedule_obj = self.pool.get('ir.cron').browse(cr, uid, line_id)
+        schedule_obj.write({'interval_number': interval_number, 'interval_type': interval_type})
 
 configure_attendence()
